2f5be28bd628226373a68775a094594705c04cf6
susumu tomita <oyster880@gmail.com>
2023-02-19 08:55:21 +0900

    add 5.3.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get  'static_pages/contact'
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root 'static_pages#home'
 end
