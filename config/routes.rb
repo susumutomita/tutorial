0ebdb366748a7e5e17ccac284c964597166efc1d
susumu tomita <oyster880@gmail.com>
2023-02-19 09:06:54 +0900

    fix rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  get  'static_pages/contact'
+  get 'static_pages/contact'
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
