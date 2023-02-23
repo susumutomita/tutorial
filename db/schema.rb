aea8f1241fb7a3f7a102cf8716272667746a8e9f
susumu tomita <oyster880@gmail.com>
2023-02-24 07:23:27 +0900

    add 6.2.5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_31_012708) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_23_222230) do
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_31_012708) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
