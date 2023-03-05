365791f94f230270bc5fb3bef25a68cd0bcfe06b
susumu tomita <oyster880@gmail.com>
2023-03-06 07:27:08 +0900

    add 7.4.4

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -22,4 +22,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       end
     end
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example User",
+                                         email: "user@example.com",
+                                         password: "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash[:danger]
+  end
 end
