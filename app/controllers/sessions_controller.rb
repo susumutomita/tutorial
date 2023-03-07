bd9621ace3984e5ae64f4ba311318b880bdcd1bb
susumu tomita <oyster880@gmail.com>
2023-03-08 07:05:53 +0900

    add 8.1.3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 # sessions
 class SessionsController < ApplicationController
   def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      # エラーメッセージを作成する
+      render 'new'
+    end
+  end
+
+  def destroy; end
 end
