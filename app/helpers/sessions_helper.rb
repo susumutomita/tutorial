4250fc680b6e8f64b06f09eb3b093a5bd51b4d13
susumu tomita <oyster880@gmail.com>
2023-03-12 08:36:11 +0900

    add 8.2.4

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,9 +7,9 @@ module SessionsHelper
 
   # 現在ログイン中のユーザーを返す (いる場合)
   def current_user
-    return unless session[:user_id]
-
-    @current_user ||= User.find_by(id: session[:user_id])
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
   end
 
   # ユーザーがログインしていればtrue、その他ならfalseを返す
