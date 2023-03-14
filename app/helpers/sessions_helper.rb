49e01920cc04742198693b3c2f215d2a613474af
susumu tomita <oyster880@gmail.com>
2023-03-15 07:34:52 +0900

    try

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,7 +9,7 @@ module SessionsHelper
   def current_user
     return unless session[:user_id]
 
-    User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   # ユーザーがログインしていればtrue、その他ならfalseを返す
