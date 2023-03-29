b204744971cc96e1dfdb6c6f6de2ab1acb923c0c
susumu tomita <oyster880@gmail.com>
2023-03-30 07:55:34 +0900

    add 10.2.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 # Users Controller
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
@@ -39,5 +40,15 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  # beforeアクション
+
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    return if logged_in?
+
+    flash[:danger] = "Please log in."
+    redirect_to login_url
   end
 end
