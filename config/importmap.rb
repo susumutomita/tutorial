e9ded437b5d9c7d84f92d32e9c4f67e9418990c2
susumu tomita <oyster880@gmail.com>
2023-04-10 07:42:41 +0900

    wip rails7

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,3 @@
 pin "jquery", to: "jquery/dist/jquery.js"
 pin "bootstrap", to: "bootstrap/dist/js/bootstrap.bundle.js"
-
-pin "application", to: "application.js"
+pin "application", to: "application.js", preload: true
