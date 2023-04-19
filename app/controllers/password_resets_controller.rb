14822749b9808210b3309647ae03788d60785439
susumu tomita <oyster880@gmail.com>
2023-04-20 07:21:56 +0900

    add 12.1.1

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,6 @@
+# Controller for the password reset functionality
+class PasswordResetsController < ApplicationController
+  def new; end
+
+  def edit; end
+end
---
+++ app/helpers/password_resets_helper.rb
@@ -0,0 +1,5 @@
+# This code is used to retrieve the user email from the password reset form
+# The email is used to retrieve the user from the database
+# The user is then used to create a password reset token
+module PasswordResetsHelper
+end
