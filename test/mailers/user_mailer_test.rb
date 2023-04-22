fd57e11a260b651d90226ed9c87e5c116f835d94
susumu tomita <oyster880@gmail.com>
2023-04-23 06:06:55 +0900

    add 12.2.2

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -7,20 +7,25 @@ class UserMailerTest < ActionMailer::TestCase
   end
 
   test "account_activation" do
-    @user.activation_token = User.new_token # この行を追加
-    mail = UserMailer.account_activation(@user)
+    user = users(:michael)
+    user.activation_token = User.new_token
+    mail = UserMailer.account_activation(user)
     assert_equal "Account activation", mail.subject
-    assert_equal [@user.email], mail.to # メールアドレスを変更
+    assert_equal [user.email], mail.to
     assert_equal ["noreply@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match user.name,               mail.body.encoded
+    assert_match user.activation_token,   mail.body.encoded
+    assert_match CGI.escape(user.email),  mail.body.encoded
   end
 
   test "password_reset" do
-    user = @user # テスト用のユーザーを取得
+    user = users(:michael)
+    user.reset_token = User.new_token
     mail = UserMailer.password_reset(user)
     assert_equal "Password reset", mail.subject
-    assert_equal ["michael@example.com"], mail.to
+    assert_equal [user.email], mail.to
     assert_equal ["noreply@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match user.reset_token,        mail.body.encoded
+    assert_match CGI.escape(user.email),  mail.body.encoded
   end
 end
