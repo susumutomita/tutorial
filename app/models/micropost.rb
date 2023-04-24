98698abe8c000c4b493398d06cf951ea563e12b7
susumu tomita <oyster880@gmail.com>
2023-04-25 06:34:14 +0900

    add 13.1.1

---
+++ app/models/micropost.rb
@@ -0,0 +1,8 @@
+# This code defines the Micropost class, which inherits from ApplicationRecord,
+# which in turn inherits from ActiveRecord::Base.
+# The Micropost class has a single association, which is defined by the belongs_to method.
+# The belongs_to method takes a symbol as an argument,
+# which represents the name of the model that the Micropost class belongs to.
+class Micropost < ApplicationRecord
+  belongs_to :user
+end
