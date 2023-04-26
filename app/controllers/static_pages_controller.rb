2d8d01fc170f13c8db57729df8ae0ee27e643caf
susumu tomita <oyster880@gmail.com>
2023-04-26 09:18:40 +0900

    add 13.3.3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,10 @@
 # Static page controller
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.build if logged_in?
+    return unless logged_in?
+
+    @micropost  = current_user.microposts.build
+    @feed_items = current_user.feed.paginate(page: params[:page])
   end
 
   def help
