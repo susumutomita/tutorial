d035012c8b6445e771e1ab82fb82514880b6bd5a
susumu tomita <oyster880@gmail.com>
2023-04-28 07:08:32 +0900

    add 13.4.2

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -9,4 +9,11 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+
+  # アップロードされた画像のサイズをバリデーションする
+  def picture_size
+    return unless picture.size > 5.megabytes
+
+    errors.add(:picture, "should be less than 5MB")
+  end
 end
